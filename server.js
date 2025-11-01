569111196c7c9e279a39f303a5fe1eae19246457
Donald Polidor <donaldpolidor@gmail.com>
2025-11-01 02:04:58 -0700

    update on my project

--- server.js
+++ server.js
@@ -5,39 +5,41 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const path = require("path");
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+require("dotenv").config();
+
+const app = express();
+const staticRoutes = require("./routes/static");
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") //not a views root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not a views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(express.static("public"))
 
-//Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+// Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home"});
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500;
+const host = process.env.HOST || "localhost";
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
